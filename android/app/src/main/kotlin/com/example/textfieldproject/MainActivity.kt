87feaf10356b4e98ea55ee6f69fa7b543bc485fe
lavanya reddaboina <lavanyaredddaboina01@gmail.com>
2022-10-22 11:29:10 +0530

    commit

---
+++ android/app/src/main/kotlin/com/example/textfieldproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.textfieldproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/textfield.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TextFieldDemo extends StatefulWidget {
+  @override
+  _TextFieldDemo createState() => _TextFieldDemo();
+}
+
+class _TextFieldDemo extends State<TextFieldDemo> {
+  bool passenable = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TextField'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: new InputDecoration(
+                  labelText: "Cursor red and maxlength ten"),
+              cursorColor: Colors.red,
+              maxLength: 10,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: TextField(
+              decoration: new InputDecoration(labelText: "Enter your number"),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.digitsOnly
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: TextField(
+              decoration: new InputDecoration(
+                  labelText: "Multiple line acceptance textfield"),
+              maxLines: null,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Border TextField",
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                ),
+              ),
+            ),
+          ),
+          TextField(
+            obscureText:
+                passenable, 
+            decoration: InputDecoration(
+                hintText: "Enter Password Here",
+                labelText: "Password",
+                suffix: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        if (passenable) {
+                          
+                          passenable = false;
+                        } else {
+                          passenable =
+                              true; 
+                        }
+                      });
+                    },
+                    icon: Icon(passenable == true
+                        ? Icons.remove_red_eye
+                        : Icons.password))
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
