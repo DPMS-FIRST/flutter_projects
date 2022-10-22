44dac41b4eb6c5222c23308e4b540af21eecbae6
lavanya reddaboina <lavanyaredddaboina01@gmail.com>
2022-10-22 11:10:05 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_application_1/LoginPage.dart';
-import 'package:flutter_application_1/SampleLogin.dart';
-import 'package:flutter_application_1/get_start.dart';
-import 'package:flutter_application_1/routes/app_pages.dart';
-import 'package:flutter_application_1/routes/app_routes.dart';
-import 'package:flutter_application_1/signup.dart';
-import 'package:flutter_application_1/textfield.dart';
 
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
