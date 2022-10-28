178e085690ce42099fc75e1656638b2fe1340224
lavanya reddaboina <lavanyaredddaboina01@gmail.com>
2022-10-28 11:27:37 +0530

    splash

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:splash/loginpage.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(LoginPage1());
+}
